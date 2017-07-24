c090aec1369d2451fffd952f158ee118d9b39f81
Sh1ft0x0EF <Sh1ft0x0EF@users.noreply.github.com>
2017-07-24 15:25:24 +0000

    Add vphysics module implementation from Garrys Mod

---
+++ dedicated_main/main.cpp
@@ -0,0 +1,17 @@
+#include <cstdlib>
+
+int main(int argc, char **argv)
+{
+	//
+	// TODO:
+	// * Load dedicated module
+	// * Get the factory func from it
+	// * Get the dedicated engine api interface from it
+	// * Init it
+	// * Per-frame update until engine state is not == quit
+	// * Shutdown
+	// * Unload
+	//
+	
+	return EXIT_SUCCESS;
+};
