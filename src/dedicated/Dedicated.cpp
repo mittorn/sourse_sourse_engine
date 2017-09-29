cfc4f55a23937e3ebfe91650c8adac2a8c47734a
Sh1ft0x0EF <Sh1ft0x0EF@users.noreply.github.com>
2017-09-29 06:07:27 +0000

    Relocate codebase to 'src' folder; Move sdk headers to another repo [ci skip]

---
+++ src/dedicated/Dedicated.cpp
@@ -0,0 +1,55 @@
+#include "interface.h"
+
+#ifdef _MSC_VER
+	#define EXPORT_FUNC [[dllexport]]
+#elif __GNUC__
+	#define EXPORT_FUNC [[visibility("default")]]
+#endif
+
+#define C_EXPORT_FUNC extern "C" EXPORT_FUNC
+
+C_EXPORT_FUNC int DedicatedMain(int argc, char **argv)
+{
+	//
+	// TODO:
+	// * Load engine module
+	// * Get the factory func from it
+	// * Get the dedicated engine api interface from it
+	// * Init it
+	// * Per-frame update until engine state is not == quit
+	// * Shutdown
+	// * Unload
+	//
+	
+	CSysModule *hEngineLib = Sys_LoadModule("engine");
+	
+	if(!hEngineLib)
+		return EXIT_FAILURE;
+	
+	CreateInterfaceFn fnEngineFactory = Sys_GetFactory(hEngineLib);
+	
+	if(fnEngineFactory)
+		return EXIT_FAILURE;
+	
+	IDedicatedServerAPI *pEngine = fnEngineFactory(VENGINE_HLDS_API_VERSION, nullptr);
+	
+	if(pEngine)
+		return EXIT_FAILURE;
+	
+	pEngine->Init();
+	
+	ModInfo_t EngineModInfo{};
+	
+	pEngine->ModInit(EngineModInfo);
+	
+	while(true)
+		pEngine->Frame();
+	
+	pEngine->ModShutdown();
+	
+	pEngine->Shutdown();
+	
+	Sys_UnloadModule(hEngineLib);
+
+	return -1; // return what?
+};
