fa6000be5d2c09c52aefe0dcdc8b52f9f0ecd05d
Sh1ft0x0EF <Sh1ft0x0EF@users.noreply.github.com>
2017-11-25 18:50:52 +0000

    Try to implement the vstdlib (3) [ci skip]

--- src/vstdlib/random.cpp
+++ src/vstdlib/random.cpp
@@ -1,5 +1,46 @@
 #include "vstdlib/random.h"
 
+CUniformRandomStream::CUniformRandomStream()
+{
+};
+
+void CUniformRandomStream::SetSeed(int iSeed)
+{
+};
+
+float CUniformRandomStream::RandomFloat(float flMinVal, float flMaxVal)
+{
+	return 0.0f;
+};
+
+int CUniformRandomStream::RandomInt(int iMinVal, int iMaxVal)
+{
+	return 0;
+};
+
+float CUniformRandomStream::RandomFloatExp(float flMinVal, float flMaxVal, float flExponent)
+{
+	return 0.0f;
+};
+
+int CUniformRandomStream::GenerateRandomNumber()
+{
+	return -1;
+};
+
+CGaussianRandomStream::CGaussianRandomStream(IUniformRandomStream *pUniformStream)
+{
+};
+
+void CGaussianRandomStream::AttachToStream(IUniformRandomStream *pUniformStream);
+{
+};
+
+float CGaussianRandomStream::RandomFloat(float flMean, float flStdDev)
+{
+	return 0.0f;
+};
+
 CDefaultUniformRandomStream gDefaultUniformRandomStream;
 IUniformRandomStream *gpUniformRandomStream{&gDefaultUniformRandomStream};
 
